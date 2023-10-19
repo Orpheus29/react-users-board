bc0d945dccda4bf4c9d4291b5f3c3f6130c76024
Oleksii Andriushyn <andriushyn.aleks@gmail.com>
2023-10-19 18:15:54 +0300

    add createUser function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { UserList } from './components/UserList';
 import { AddUserForm } from './components/AddUserForm';
 import { AppContainer } from './components/AppContainer';
 import { Color, UserWithColor } from './types';
 import { prepareUsers } from './helpers';
-import { getUsers } from './services/users.service';
+import { createUser, getUsers } from './services/users.service';
 import { getColors } from './services/colors.service';
 
 export const App: React.FC = () => {
@@ -24,17 +24,17 @@ export const App: React.FC = () => {
     });
   }, []);
 
-  const addUser = useCallback((name: string, carColorId: number) => {
+  const addUser = async (name: string, carColorId: number) => {
+    const response = await createUser({ name, carColorId });
     const color = colors.find(c => c.id === carColorId);
-    const newUser: UserWithColor = {
-      id: Math.random(),
-      carColorId,
-      name,
+
+    const newUser = {
+      ...response,
       carColor: color,
     };
 
     setUsers((prev) => [...prev, newUser]);
-  }, []);
+  };
 
   return (
     <AppContainer>
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -4,3 +4,10 @@ import { User } from '../types';
 export function getUsers() {
   return client.get<User[]>('/users');
 }
+
+export function createUser({ name, carColorId }: Omit<User, 'id'>): Promise<User> {
+  return client.post<User>('/users', {
+    name,
+    carColorId,
+  });
+}
